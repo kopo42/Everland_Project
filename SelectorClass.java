0c0ee61fbba765f2b24b6bd9fb41f3846ddf5b15
kopo42 <jych41@gmail.com>
2021-04-26 11:22:48 +0900

    second upload

--- SelectorClass.java
+++ SelectorClass.java
@@ -1,5 +1,20 @@
 package everland;
 
-public class SelectorClass {
+import java.util.Scanner;
 
+public class SelectorClass {
+	ProcessingClass pc = new ProcessingClass();
+	
+	public void timeSelect(TypeClass t) {
+		pc.getTime(t);
+	}
+	
+	public void ageSelect(TypeClass t) {
+		pc.getAge(t);
+	}
+	
+	public void discntSelect(TypeClass t) {
+		pc.getDiscnt(t);
+	}
 }
+
